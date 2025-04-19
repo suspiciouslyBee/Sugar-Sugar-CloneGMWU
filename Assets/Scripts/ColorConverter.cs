1b02c8b1b088c3a6d37c95bac473befdb7f01f89
suspiciouslyBee <suspiciously@bee.movie>
2025-04-19 15:50:24 -0700

    + updatebasecolor() so that i can update from the root prefab instance level

--- Assets/Scripts/ColorConverter.cs
+++ Assets/Scripts/ColorConverter.cs
@@ -1,13 +1,24 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ColorConverter : MonoBehaviour
 {
     public GameObject converterBar;
+    public List<SpriteRenderer> nubList;
+
+    public Color colorToConvertTo = Color.white;
+    public float nubColorOffset = 0.2f;
+
+
+    //left private as i dont need to know this for editing
+    
+    private Color nubColor = Color.white;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //converterBar.
     }
 
     // Update is called once per frame
@@ -18,7 +29,34 @@ public class ColorConverter : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        collision.GetComponent<SpriteRenderer>().color 
-            = converterBar.GetComponent<SpriteRenderer>().color;
+        collision.GetComponent<SpriteRenderer>().color = colorToConvertTo;
     }
+
+    //Validate runs every time something is changed in the inspector
+    //Basically automates assigning a color
+    private void OnValidate()
+    {
+        UpdateBaseColor();
+    }
+
+    //seperated from OnValidate incase I'd like to run this elsewhere in the future
+    void UpdateBaseColor()
+    {
+        //Raw color to the Bar/Gate
+        converterBar.GetComponent<SpriteRenderer>().color = colorToConvertTo;
+
+        //Calc the nubColor now
+        nubColor = colorToConvertTo;
+        nubColor -= new Color(nubColorOffset, nubColorOffset, nubColorOffset);
+        nubColor.a = 1;
+
+        //Apply offset to Nubs, ballasts, idk what to call them
+        
+        foreach (SpriteRenderer nub in nubList)
+        {
+            nub.color = nubColor;
+        }
+
+    }
+
 }
