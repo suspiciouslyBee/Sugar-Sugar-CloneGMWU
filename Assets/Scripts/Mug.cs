c1193a53876162e2bd96bcf144844711ce5ddbba
suspiciouslyBee <suspiciously@bee.movie>
2025-04-19 10:28:17 -0700

    added improved color checking, and mug capacity

--- Assets/Scripts/Mug.cs
+++ Assets/Scripts/Mug.cs
@@ -1,30 +1,46 @@
+using TMPro;
 using UnityEngine;
 
 public class Mug : MonoBehaviour
 {
+    //bake
+    public int mugRemaining = 100;
+    public TextMeshProUGUI scoreTextObj; //just store the ref
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        scoreTextObj.text = mugRemaining.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        scoreTextObj.text = mugRemaining.ToString();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         //if && works like C/C++, the object tag will get checked first
-        //mouthful, just checking if the collider is a particle, then checking if our (mug's) color
-        //matches
 
-        if(collision.CompareTag("Particle") 
-            && collision.GetComponent<SpriteRenderer>().color 
-            == gameObject.GetComponent<SpriteRenderer>().color)
+        //order of elimination:
+        //mugRemaining (broadest) - will not accept anything if < 1
+        //only accepts particles
+
+        //the original Sugar Sugar game would accept/destroy all particles at this point, but
+        //wouldnt decrement the counter. so ive replicated this with a nested if
+
+
+        if (mugRemaining > 0 && collision.CompareTag("Particle"))
         {
+            if (collision.GetComponent<SpriteRenderer>().color
+                == gameObject.GetComponent<SpriteRenderer>().color)
+            {
+                mugRemaining--;
+            }
+
             Destroy(collision.gameObject);
+
         }
     }
 }
